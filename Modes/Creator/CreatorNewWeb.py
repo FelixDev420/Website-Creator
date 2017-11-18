506f05a769efe0aff0099912451570cb7678b8fa
GamingTrend <rafeole123@gmail.com>
2017-11-18 11:49:40 +0100

    Create CreatorNewWeb.py

---
+++ Modes/Creator/CreatorNewWeb.py
@@ -0,0 +1,17 @@
+print('Welcome to the Website Creator!')
+print('Start now!')
+if a == Start now!
+   import SimpleHTTPServer
+    import SocketServer
+
+     PORT = 8000
+
+      Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+        httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+         print "Server during...", PORT
+          httpd.serve_forever()
+name = input('Which name should your website have: ')
+   SimpleHTTPServer = https://www.'name'.com
+   print('Your Website is now online, go now to the editor')
